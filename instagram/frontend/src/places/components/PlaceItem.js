8bad35b3a52cf43dbd287f7b65550e5eb6491351
Aleksandar Ilic <aleksandar.ilic@htecgroup.com>
2024-01-09 12:46:40 +0100

    feat: custom button component

--- instagram/frontend/src/places/components/PlaceItem.js
+++ instagram/frontend/src/places/components/PlaceItem.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import Card from '../../shared/components/UIElements/Card';
-import './PlaceItem.css';
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import "./PlaceItem.css";
 
-const PlaceItem = props => {
-
+const PlaceItem = ({
+  id,
+  image,
+  title,
+  description,
+  address,
+  creatorId,
+  coordinates,
+}) => {
   return (
-      <li className="place-item">
-        <Card className="place-item__content">
-          <div className="place-item__image">
-            <img src={props.image} alt={props.title} />
-          </div>
-          <div className="place-item__info">
-            <h2>{props.title}</h2>
-            <h3>{props.address}</h3>
-            <p>{props.description}</p>
-          </div>
-          <div className="place-item__actions">
-            <button>
-              VIEW ON MAP
-            </button>
-              <button>EDIT</button>
-              <button>DELETE</button>
-          </div>
-        </Card>
-      </li>
+    <li className="place-item">
+      <Card className="place-item__content">
+        <div className="place-item__image">
+          <img src={image} alt={title} />
+        </div>
+        <div className="place-item__info">
+          <h2>{title}</h2>
+          <h3>{address}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="place-item__actions">
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
+        </div>
+      </Card>
+    </li>
   );
 };
 
